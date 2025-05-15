9c4b3c7e1a5127d1e833c72b74e239248e22b600
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2025-05-15 10:26:40 +0300

    Update CORS configuration to allow requests from the production URL

--- api/index.js
+++ api/index.js
@@ -21,7 +21,8 @@ app.use(cookieParser());
 app.use(
    cors({
       credentials: true,
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173","https://evento-ems-guyl.onrender.com/"]
+
    })
 );
 
